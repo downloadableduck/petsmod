635c4d0e9b057aa4e51cddc47b270ff0ee6c7232
downloadableduck <238381293+downloadableduck@users.noreply.github.com>
2026-09-14 15:43:06 -0700

    - add option to always display pet nametags - add option to disable rendering of pet hitboxes - fixed pets wandering too far and teleporting back

--- src/main/java/com/jeff/pets/client/rendering/PetRenderer.java
+++ src/main/java/com/jeff/pets/client/rendering/PetRenderer.java
@@ -128,4 +128,12 @@ public abstract class PetRenderer<D extends AbstractPet, U extends LivingEntityR
     public String getPetSkin(String traitorSkin) {
         return traitorSkin;
     }
+
+    @Override
+    public boolean shouldShowName(D entity, double distanceToCamera) {
+        if (CONFIG.alwaysRenderNametag && !(Minecraft.getInstance().gui.screen() instanceof PetsConfigScreen)) {
+            return true;
+        }
+        return super.shouldShowName(entity, distanceToCamera);
+    }
 }
