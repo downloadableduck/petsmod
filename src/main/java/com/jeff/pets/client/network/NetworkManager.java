53f831520ca31babfbae3720b1109b5e9d9da509
downloadableduck <238381293+downloadableduck@users.noreply.github.com>
2026-09-20 13:09:08 -0700

    - Reverted the Ably bump, moving back from 1.8.2 over to 1.2.40 because of the amount of issues it caused. - This includes a crash, a disconnect on lobby change, and another disconnect on lobby change. I was unfortunately not able to solve the last one and decided that reverting the bump would be the best idea for an immediate hotfix. We may or may not try and increase the Ably version again in the future, but we will do so more carefully if we do. - Fix an issue with the client ID causing players to not be able to see each other's pets.

--- src/main/java/com/jeff/pets/client/network/NetworkManager.java
+++ src/main/java/com/jeff/pets/client/network/NetworkManager.java
@@ -47,11 +47,6 @@ public class NetworkManager {
             String channelName = "petsmod:server:" + this.getIp(ip);
             this.log(channelName);
             channel = ably.channels.get(channelName);
-            try {
-                this.options.clientId = Minecraft.getInstance().player.getName().getString();
-            } catch (NullPointerException e) {
-                this.options.clientId = "Unknown";
-            }
             channel.subscribe("requestPetState", (message) -> {
                 try {
                     this.log("Received a request for current pet state");
@@ -207,7 +202,9 @@ public class NetworkManager {
 
     public void disconnect() {
         this.log("Disconnected.");
-        ably.close();
+        if (ably != null) {
+            ably.close();
+        }
     }
 
     public void broadcastGeneral(String playerUuid,
