1a56b8f007a3e12c1f3a5f346e8d6c9c38faa421
downloadableduck <238381293+downloadableduck@users.noreply.github.com>
2026-09-22 10:58:15 -0700

    fixed an incompatbility with jei

---
+++ src/main/java/com/jeff/pets/client/mixin/client/JEIPatcher.java
@@ -0,0 +1,40 @@
+package com.jeff.pets.client.mixin.client;
+
+import net.minecraft.core.Holder;
+import net.minecraft.data.loot.EntityLootSubProvider;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.level.storage.loot.LootTable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+@Mixin(EntityLootSubProvider.class)
+public class JEIPatcher {
+    @Shadow
+    @Final
+    private FeatureFlagSet required;
+
+    @Shadow
+    @Final
+    private Map<EntityType<?>, Map<ResourceKey<LootTable>, LootTable.Builder>> map;
+
+    @Inject(at = @At(value = "HEAD"), method = "lambda$run$0", require = 0, cancellable = true)
+    private void pets$onCreateRegistryProvider(Set seen, Holder.Reference holder, CallbackInfo ci) {
+        EntityType<?> type = (EntityType<?>) holder.value();
+        Map<ResourceKey<LootTable>, LootTable.Builder> builders = (Map)this.map.get(type);
+
+        Optional<ResourceKey<LootTable>> defaultLootTable = type.getDefaultLootTable();
+        if (type.isEnabled(this.required) && (builders == null || !builders.containsKey(defaultLootTable.get()))) {
+            ci.cancel();
+        }
+    }
+}
